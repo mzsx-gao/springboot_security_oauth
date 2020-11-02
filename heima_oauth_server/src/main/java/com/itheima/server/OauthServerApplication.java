b845e25b8cabdec437f83f39f681a3ccd4ea2db9
mzsx-gao <18818196720@613.com>
2020-11-02 17:58:26 +0800

    优化代码结构

--- heima_oauth_server/src/main/java/com/itheima/server/OauthServerApplication.java
+++ heima_oauth_server/src/main/java/com/itheima/server/OauthServerApplication.java
@@ -3,13 +3,15 @@ package com.itheima.server;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
 @MapperScan("com.itheima.server.mapper")
 /*
- * EnableResourceServer注解开启资源服务，因为程序需要对外暴露获取token的API和
- * 验证token的API所以该程序也是一个资源服务器
+ * EnableResourceServer注解开启资源服务，因为程序需要对外暴露获取token的API和验证token的API,
+ * 所以该程序也是一个资源服务器
  * */
+@EnableResourceServer
 public class OauthServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(OauthServerApplication.class, args);
